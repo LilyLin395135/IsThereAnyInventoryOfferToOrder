3d45979fb0e963192236659d0f6c2ac9dfc6a9dd
lily <s395135@sinyi.com.tw>
2023-06-10 18:03:06 +0800

    學長方法四：用工具NSubstitute。

--- June10thPractice/CheckInventoryTest.cs
+++ June10thPractice/CheckInventoryTest.cs
@@ -1,6 +1,7 @@
 using ClassLibrary1;
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
 
 namespace IsThereAnyInventoryOfferToOrder
 {
@@ -13,7 +14,12 @@ namespace IsThereAnyInventoryOfferToOrder
         [TestMethod]
         public void ToGetDeny()
         {
-            var fakeinventory = new FakeInventory();
+            var fakeinventory = Substitute.For<Inventory>();
+            //1.代表指定空的OrderItems就會回傳false↓
+            //fakeinventory.CheckInventory(new OrderItems()).Returns(false);
+            //2.代表指定Name是"Product 1"的回傳false。//o => o.Name == "Product 1"【o代表前面的OrderItems】
+            //fakeinventory.CheckInventory(Arg.Is<OrderItems>(o => o.Name == "Product 1")).Returns(false);
+            fakeinventory.CheckInventory(Arg.Any<OrderItems>()).Returns(false);
             var orderProcessor = new OrderProcessor(fakeinventory);
 
             var order = new Order();
@@ -23,8 +29,6 @@ namespace IsThereAnyInventoryOfferToOrder
             {
                 new OrderItems { Id = 1, Name = "Product 1", Quantity = 2 }
             };
-
-            fakeinventory.IsThereAnyInventory = false;
 
             orderProcessor.ProcessOrder(order);
 
@@ -41,18 +45,16 @@ namespace IsThereAnyInventoryOfferToOrder
             [TestMethod]
             public void ToGetProcessed()
             {
-                var fakeinventory = new FakeInventory();
+                var fakeinventory = Substitute.For<Inventory>();
+                fakeinventory.CheckInventory(Arg.Any<OrderItems>()).Returns(true);
                 var orderProcessor = new OrderProcessor(fakeinventory);
 
                 var order = new Order();
                 order.OrderNo = "1";
-                //Item要做假資料
                 order.Items = new System.Collections.Generic.List<OrderItems>
             {
                 new OrderItems { Id = 1, Name = "Product 1", Quantity = 2 }
             };
-
-                fakeinventory.IsThereAnyInventory = true;
 
                 orderProcessor.ProcessOrder(order);
 
@@ -61,6 +63,7 @@ namespace IsThereAnyInventoryOfferToOrder
             }
         }
 
+        //如果放入四個參數進去依賴注入，就要override四個，所以這裡可以簡化
         public class FakeInventory: Inventory
         {
 
