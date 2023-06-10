5d441c625029a39c3df2a99dd05e31069e7ef466
lily <s395135@sinyi.com.tw>
2023-06-10 10:58:14 +0800

    加入專案檔案。

---
+++ June10thPractice/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace June10thPractice
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
